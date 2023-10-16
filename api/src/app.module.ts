63d04c1e06a484b7e0fede5aeceebfcc7e28baba
phillimong <philiplimongetich@Gmail.com>
2023-10-16 10:10:40 +0300

    feat: authentication

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,13 +3,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
 //enable dotenv
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 @Module({
-  imports: [MongooseModule.forRoot(`${process.env.MONGO_URI}`), UsersModule],
+  imports: [
+    MongooseModule.forRoot(`${process.env.MONGO_URI}`),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { UserSchema } from '../users/users.model';
+
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.auth';
+
+//enable dotenv
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secret',
+      signOptions: { expiresIn: '1h' },
+    }),
+    MongooseModule.forFeature([{ name: 'user', schema: UserSchema }]),
+  ],
+  providers: [UsersService, AuthService, LocalStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
